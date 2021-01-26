ae043834eff919f6acc12149f9d459d033869eed
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-27 01:33:58 +0200

    Variables

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello")
+# Variables
+name = "Jamila"
+first, second = "Mike", 27
+age = 20
+number: int = 13
+pi = 3.14
+numbers = [1, 2, 3, 4, 5]
+
+print(name)
+print(first)
+print(second)
+print(age)
+print(number)
+print(pi)
+print(numbers)
