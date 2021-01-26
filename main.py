9a3093975f20dfbf857f7aed3338493216204a71
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-27 01:50:40 +0200

    Single and Multiline Comments

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-# Dynamically Type Language
+# First
+# Second
 
-brand = "Some Brand"
-
-name: str = "Dan"
-isEnable: bool = False
-
-
-def hello():
-    return "word"
-
-
-def bye() -> int:
-    return 7
-
+"""
+Multiline
+Comment
+"""
