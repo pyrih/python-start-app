6a93e949559e79a278ecac1fac49e06edd2ac8b2
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-27 01:37:31 +0200

    Naming Variables

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-# Variables
-name = "Jamila"
-first, second = "Mike", 27
-age = 20
-number: int = 13
-pi = 3.14
-numbers = [1, 2, 3, 4, 5]
-
-print(name)
-print(first)
-print(second)
-print(age)
-print(number)
-print(pi)
-print(numbers)
+name = "James"
+last_name = "Smith"
+fullName = "J James"
+PI = 3.14
