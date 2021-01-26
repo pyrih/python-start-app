82371ecee187c7f1cf3bcbb7de153a308ed87d48
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-27 01:41:31 +0200

    Data Types

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-name = "James"
-last_name = "Smith"
-fullName = "J James"
-PI = 3.14
+# Data types
+
+brand = "Brand"
+age = 7
+pi = 3.14
+numbers = []
+isAdult = True
+
+print(type(brand))
+print(type(age))
+print(type(pi))
+print(type(numbers))
+print(type(isAdult))
