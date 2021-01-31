6bfa91527ec39c8725f3347785139d9a38ef1f7d
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-31 23:50:14 +0200

    comparison operators

--- main.py
+++ main.py
@@ -1,34 +1,8 @@
-# INDENTATION
+# comparison operators
 
-import keyword
-
-name = "Name"
-surname = "Surname"
-
-
-def my_func():
-    name2 = "Name"
-    surname2 = "Surname"
-
-
-# RESERVED KEYWORDS
-
-print(keyword.kwlist)
-# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await',
-# 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except',
-# 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
-# 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
-
-# ARITHMETIC OPERATIONS
-
-result = 1 + 2
-print(result)
-print(13 + 7)
-print(13 * 7)
-print(10 / 5)
-print(9 % 2)
-print(3 ** 2)
-
-
-
-
+print(10 > 5)
+print(10 < 5)
+print(10 <= 5)
+print(10 >= 5)
+print(10 == 5)
+print(10 != 5)
