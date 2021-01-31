2793fc6d56059350153d24f6d7137887e0533894
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-31 23:56:35 +0200

    assignment operators

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-# logical operators
+# ASSIGNMENT OPERATORS
 
-print((10 > 5) and (7 < 9) or "A" != "B")
-print(not ("A" == "C"))
+brand = "Andrii"
+brand = "Pyrih"
+print(brand)
 
-
+number = 0
+number += 2  # number = number + 2
+number -= 1
+number *= 10
+number /= 5
+number **= 3
+print(number)
